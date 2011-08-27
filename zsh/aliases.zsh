185b7ab267605a7f3b850e76f8b993cef4a6323d
Jessica Lynn Suttles <jlsuttles@gmail.com>
2011-08-27 13:16:43 -0700

    cleans up aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,22 +14,22 @@ alias fx='git fetch && gitx'
 alias giff='git diff | gitx'
 alias gitx='gitx --all'
 
+alias easy_off='sudo kextunload -v /System/Library/Extensions/EasyTetherUSBEthernet.kext'
+
 alias c='bundle exec cucumber'
 alias s='bundle exec rspec'
-alias redis= 'redis-server > /Users/jessicasuttles/redis.log &'
+alias redis= 'redis-server > /Users/jlsuttles/redis.log &'
 alias myip="ifconfig | grep 'inet ' | grep -v 127.0.0.1 | 
    cut -d\   -f2"
 
+# reloads passenger and pow
 function reload! () {
   touch tmp/restart.txt
 }
 
+# better than rm -rf
 function trash () {
   mv $* ~/.Trash
-}
-
-function gco () {
-  git checkout $*
 }
 
 # cd to the default working directory set by current_working_project
@@ -43,54 +43,19 @@ function current_working_project {
 }
 cdefault
 
-function cdproject { 
-	cd $HOME/projects/$* 
-}
-compctl -W "$HOME/projects" -g '*(-/)' cdproject
-
-
-function sp {
-  bundle exec rspec -cfs spec/$*
-}
-compctl -W "spec" -g '*' sp
-
-
-function setIcon () {
-  echo "read 'icns' (-16455) \"${1}\";" | /Developer/Tools/Rez -o ${2}
-  /Developer/Tools/SetFile -a "C" ${2}
-}
-
-function follow () {
-  tail -n 0 -f $*
-}
-
-function cuke () {
-  if [[ -e ./script/cucumber ]]
-  then
-    ./script/cucumber -f progress features/$*
-  else
-    cucumber -f progress features/$*
-  fi
-}
-compctl -W "features" -f cuke
-compctl -W "$HOME/projects" -g '*(-/)' cdproject
-
 function :w () {
   echo "Ugh. You're not in vim, and your shits all retarded"
 }
 
-function v () {
-   ssh vm "$*"
-}
-
-function topn () {
-  top -n ${1} -l 1 | tail -n ${1} | awk 'BEGIN{ORS=","}{print $1}' | xargs ps -p
-}
-
-function vack () {
-    mvim -p $(ack -l $@ | xargs) &> /dev/null &
-}
-
 function internet {
-  (ping -c 3 -t 3 google.com >/dev/null 2>&1 && echo 'yep') || echo 'nope'
+  # count 3 packets
+  # timeout 3 seconds
+  # /dev/null unix devices that doesn't go anywhere
+  # 1 is stdout, 2 is stderr, 2 follow 1
+  if (ping -c 3 -t 3 google.com > /dev/null 2>&1)
+  then
+    echo 'yep'
+  else
+    echo 'nope'
+  fi
 }
