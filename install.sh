08a857277acda617a1e03482a47bb00b6a78d776
Ehren Murdick <pair+ehren@pivotallabs.com>
2014-04-18 11:22:13 -0700

    better installing

--- install.sh
+++ install.sh
@@ -1,2 +1,45 @@
-curl https://raw.github.com/ehrenmurdick/config/master/gvimrc > ~/.gvimrc
-curl https://raw.github.com/ehrenmurdick/config/master/vimrc > ~/.vimrc
+promptyn () {
+    while true
+    do
+        read -p "$1 [y/n] " yn
+        case $yn in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer y or n";;
+        esac
+    done
+}
+
+install() {
+  echo "https://raw.github.com/ehrenmurdick/config/master/$1 > ~/.$1"
+}
+
+safeInstall() {
+  if [ -f ~/.$1 ]; then
+    if promptyn "file ~/.$1 exists. overwrite?"; then
+      install $1
+      return 0;
+    fi
+  fi
+  return 1;
+}
+
+promptInstall() {
+  if promptyn "install $1?"; then
+    if safeInstall $1; then
+      echo $2
+      echo
+      return 0;
+    else
+      echo
+      return 1;
+    fi
+  else
+    echo
+    return 1;
+  fi
+}
+
+
+promptInstall 'vimrc' 'Run :BundleInstall the first time you start vim. Happy hacking!'
+promptInstall 'gvimrc'
