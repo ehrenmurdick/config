847ccb9cb9d32c2486ef15c74d1648b06bf8c4e4
Ehren Murdick <emurdick@pivotal.io>
2016-03-25 11:47:58 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,6 +28,8 @@ git config --global core.editor /usr/bin/vim
 git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit"
 git config --global alias.story '!f() { n=$1; story=$(sed -e "s/#//" <<< $n); branch=$(git branch -a | grep -o "$story[-_a-zA-Z]*" | head -n 1); git checkout $branch; }; f'
 git config --global alias.cb "rev-parse --abbrev-ref HEAD"
+git config --global core.excludesfile '~/.gitignore_global'
+git config --global alias.ci commit
 
 safeInstall 'vimrc'
 safeInstall 'gvimrc'
