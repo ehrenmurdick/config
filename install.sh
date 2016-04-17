094b3338ea1833e670a32758a25dde6c354eca72
Ehren Murdick <pair+emurdick@pivotallabs.com>
2016-04-17 15:35:17 -0700

    update install.sh to not be so aggressive

--- install.sh
+++ install.sh
@@ -15,12 +15,6 @@ backup() {
   cp ~/.$1 ~/.$1~
 }
 
-safeClone() {
-  if [ ! -d $2 ]; then
-    git clone $1 $2
-  fi
-}
-
 safeInstall() {
   echo "Installing $1..."
   if [ -f ~/.$1 ]; then
@@ -32,25 +26,22 @@ safeInstall() {
 }
 
 echo "Configuring git to not suck..."
-git config --global alias.st status
+git config --global alias.cb "rev-parse --abbrev-ref HEAD"
+git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.dci duet-commit
-git config --global core.editor /usr/bin/vim
 git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit"
+git config --global alias.pl "pull --ff-only"
+git config --global alias.st status
 git config --global alias.story '!f() { n=$1; story=$(sed -e "s/#//" <<< $n); branch=$(git branch -a | grep -o "$story[-_a-zA-Z]*" | head -n 1); git checkout $branch; }; f'
-git config --global alias.cb "rev-parse --abbrev-ref HEAD"
+git config --global core.editor /usr/bin/vim
 git config --global core.excludesfile '~/.gitignore_global'
-git config --global alias.ci commit
 
 which -s brew
 if [[ $? != 0 ]] ; then
-    echo 'Install homebrew...'
-    # Install Homebrew
-    # https://github.com/mxcl/homebrew/wiki/installation
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    echo 'Update homebrew...'
-    brew update
+    echo 'You must install homebrew'
+    echo 'http://brew.sh'
+    exit 1
 fi
 
 echo 'install ack...'
@@ -61,24 +52,16 @@ brew install dockutil
 
 which -s rbenv
 if [[ $? != 0 ]] ; then
-  echo 'Installing rbenv...'
-  brew install rbenv
+  echo 'You must install rbenv'
+  echo 'brew install rbenv'
+  exit 1
 fi
-
-echo 'Cloning bash-it...'
-safeClone 'https://github.com/Bash-it/bash-it.git' ~/.bash_it
 
 safeInstall 'vimrc'
 safeInstall 'gvimrc'
 safeInstall 'irbrc'
 safeInstall 'ackrc'
 safeInstall 'bash_profile'
-
-echo 'update some bash-it'
-bash-it enable plugin fasd
-bash-it enable completion fasd
-bash-it enable completion git
-bash-it enable completion rake
 
 echo 'Installing bundles...'
 vim +PluginInstall +qa
